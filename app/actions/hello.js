bfbba4a887907f151a9e8e9199f121e1a4ec3e9e
Soham Pramanik <clashersoham07@gmail.com>
2026-01-29 17:42:13 +0530

    check

--- app/actions/hello.js
+++ app/actions/hello.js
@@ -1,28 +1,30 @@
-const { fs } = t.core;
+// const { fs } = t.core;
 
-// preload template once
-const TEMPLATE = fs.readFile("./static/index.html") || "<h1>No Template</h1>";
-// renderer
-function render(template, data) {
-    let out = template;
-    for (const k in data) {
-        out = out.replaceAll(`tpl{{${k}}}`, data[k]);
-    }
-    return out;
-}
+// // preload template once
+// const TEMPLATE = fs.readFile("./static/index.html") || "<h1>No Template</h1>";
+// // renderer
+// function render(template, data) {
+//     let out = template;
+//     for (const k in data) {
+//         out = out.replaceAll(`tpl{{${k}}}`, data[k]);
+//     }
+//     return out;
+// }
 
 export const hello = () => {
     // fetch dog every request
-    const res = drift(
-        t.fetch("https://dog.ceo/api/breeds/image/random")
-    );
+    // const res = drift(
+    //     t.fetch("https://dog.ceo/api/breeds/image/random")
+    // );
 
-    const data = JSON.parse(res.body);
+    // const data = JSON.parse(res.body);
 
-    const html = render(TEMPLATE, {
-        name: "Shoya",
-        dog_image: data.message
-    });
+    // const html = render(TEMPLATE, {
+    //     name: "Shoya",
+    //     dog_image: data.message
+    // });
 
-    return t.response.html(html);
+    // return t.response.html(html);
+
+    return "hii"
 };
